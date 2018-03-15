36931555c8df13ead67cbb482f466d1f1d6b65fb
Chris Holmes <chrisholmes1226@gmail.com>
2018-03-15 17:41:44 +0000

    update DB url

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-//mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://yelpcamp:chris@ds115569.mlab.com:15569/yelpcamp");
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://yelpcamp:chris@ds115569.mlab.com:15569/yelpcamp");
+
+console.log(process.env.DATABASEURL);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
