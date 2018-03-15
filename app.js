c7c0a56152c0edfe1fa2d49ef0bdfecd8a77f4b4
Chris Holmes <chrisholmes1226@gmail.com>
2018-03-15 17:33:16 +0000

    mongo server

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
+//mongoose.connect("mongodb://localhost/yelp_camp_v10");
+mongoose.connect("mongodb://yelpcamp:chris@ds115569.mlab.com:15569/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
